eb95bfdedbc17d648a4eeef7f9cb613dc493e775
zlc651885461 <zhailingchao@foxmail.com>
2020-11-11 16:59:02 +0800

    添加方案文件6

--- Main.py
+++ Main.py
@@ -1,14 +1,12 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication,QMainWindow
-from ui.DemoNew import Ui_MainWindow as dn
 from ui.fangan import Ui_MainWindow as fy
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = QMainWindow()
-    ui_dn = dn()
     ui_fy = fy()
     # 事件绑定
     # ui.ReadExcelButton.clicked.connect(ui.openfile())
